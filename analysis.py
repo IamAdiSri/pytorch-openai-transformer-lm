96f07acd1ce8e9537e4f90f5bb4051712a3b21d8
Aditya Srivastava <adi.srivastava@hotmail.com>
2019-05-17 03:43:19 +0530

    fixed datasets.py, modified analysis.py for msrpc

--- analysis.py
+++ analysis.py
@@ -5,14 +5,14 @@ import pandas as pd
 
 from sklearn.metrics import accuracy_score
 
-from datasets import _rocstories
+from datasets import _msrpc
 
-def rocstories(data_dir, pred_path, log_path):
+def msrpc(data_dir, pred_path, log_path):
     preds = pd.read_csv(pred_path, delimiter='\t')['prediction'].values.tolist()
-    _, _, _, labels = _rocstories(os.path.join(data_dir, 'cloze_test_test__spring2016 - cloze_test_ALL_test.csv'))
+    _, _, labels = _msrpc(os.path.join(data_dir, 'msr_paraphrase_test.txt'))
     test_accuracy = accuracy_score(labels, preds)*100.
     logs = [json.loads(line) for line in open(log_path)][1:]
     best_validation_index = np.argmax([log['va_acc'] for log in logs])
     valid_accuracy = logs[best_validation_index]['va_acc']
-    print('ROCStories Valid Accuracy: %.2f'%(valid_accuracy))
-    print('ROCStories Test Accuracy:  %.2f'%(test_accuracy))
+    print('MSR Paraphrase Corpus Valid Accuracy: %.2f'%(valid_accuracy))
+    print('ROC Paraphrase Corpus Test Accuracy:  %.2f'%(test_accuracy))
